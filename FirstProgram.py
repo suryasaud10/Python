c1efa1feeb5b64b2e59ca58adb7272a1358baf3a
suryasaud10 <sssaud2058@gmail.com>
2025-05-08 19:31:49 +0545

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,6 @@
+print("Surya Saud")
+print("Hello World")
+
+print("My name is surya","I am 23 years old")
+print(23)
+print(23+23)
---
+++ operators.py
@@ -0,0 +1,60 @@
+#arithematic Operators
+
+a=23
+b=20
+
+sum=a+b
+print(sum)
+
+# difference
+diff= a-b
+print(diff)
+
+# multiply
+mul= a*b
+print(mul)
+
+
+# divide
+div=a /  b
+print(div)
+
+# modulo--remainder
+modulo = a%b
+print(modulo)
+
+# power operator
+
+pow=a**b
+print(pow)
+
+# Relational/ Comparision Operator
+
+x=20
+y=30 
+print(x==y)
+# false
+
+print(x!=y)
+# true
+
+print(x>y)
+# f
+
+print(x>=y)
+# f
+
+print(x<y)
+# t
+
+print(x<=y)
+# t
+
+# assignment operator
+n=10
+n=n+10
+n+=10
+print("num:",n)
+
+n*=3
+print("num:",n)
