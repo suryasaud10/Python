284db90c95938e8fbbdd06b3d65bcb7a65ffcc7b
suryasaud10 <sssaud2058@gmail.com>
2025-05-09 21:13:33 +0545

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,39 @@
+name="surya"   
+print(name)
+
+
+name1='surya'
+print(name1)
+
+
+name2='''surya'''
+print(name2)
+
+
+
+age=23
+old=False
+a=None
+print(type(age))
+print(type(old))
+print(type(a))
+
+#sum
+a=2
+b=3
+sum=a+b
+print(sum)
+
+# difference
+a=2
+b=3
+diff=a-b
+print(diff)
+
+# = single line comment
+
+# control and forward slash is used as comment and uncomment selected lines
+
+"""
+    multi line comment
+"""
