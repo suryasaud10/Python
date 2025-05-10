3654b9cc24ddc0866bb05f8d85e7471904ea2be3
Surya Saud <sssaud2058@gmail.com>
2025-05-10 22:34:12 +0545

    Add files via upload

---
+++ type_conversion.py
@@ -0,0 +1,26 @@
+# type conversion
+
+
+
+c = "3"
+d = 3
+sub = int(c)-d
+print(sub)
+
+# iput
+a=input("Enter your name:")
+print(type(a),a)
+
+# input 2 number and sum
+
+x=int(input("enter first no:"))
+y=float(input("enter second no:"))
+s=x+y
+print(s)
+l=int(input("enter the length of squre:"))
+A=l*l
+print("Area of square is:",A)
+
+p=int(input("enter first no:"))
+q=float(input("enter second no:"))
+print(p>=q)
