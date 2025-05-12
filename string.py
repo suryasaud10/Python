89d02b3c5c8298c239f04cc8b8cf16cc58223a8d
Surya Saud <sssaud2058@gmail.com>
2025-05-12 09:11:05 +0545

    Add files via upload

---
+++ string.py
@@ -0,0 +1,23 @@
+s1 = "Surya"
+print(len(s1))
+
+s2 = "Saud"
+print(len(s2))
+
+s=s1+" "+s2
+print(s)
+print(len(s))
+
+# indexing
+print(s[2])
+
+# slicing
+print(s[2:])
+print(s[:9])
+
+
+name = input("Enter your Name:")
+print("The length of my name is:",len(name))
+
+str = "Hi $i am the $ symbol"
+print(str.count("$"))
