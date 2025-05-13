e18349e606910f83ed4ee4a9ffcc471d502f94a1
Surya Saud <sssaud2058@gmail.com>
2025-05-13 22:39:59 +0545

    Add files via upload

---
+++ list.py
@@ -0,0 +1,26 @@
+# # list
+marks = [12,23,34,45,56,56,67,78,89,90]
+print(type(marks))
+print(len(marks))
+print(marks[2])
+print(marks[0:5])
+print(marks[5:10]) 
+
+# list.append
+list = [10, 20, 30, 40, 50]
+list.append(5)
+print(list)
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
+list.reverse()
+print(list)
+list.insert(15,5)
+print(list)
+list.remove(10)
+print(list)
+
+list.pop(1)
+print(list)
+
