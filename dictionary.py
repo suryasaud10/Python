030c4198ef92451a82b4c3b20254b482d6ea1c5a
Surya Saud <sssaud2058@gmail.com>
2025-05-14 23:01:08 +0545

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+# info = {
+#     "name" : "Surya",
+#     "learning" : "code",
+#     "age" : 23,
+# }
+# print(type(info) )
+
+# nested dictionary
+student =   {
+    "name" : "Raja",
+    "subject" : {
+        "chem": 67,
+        "physics": 85,
+    }
+}
+print(student["subject"]["chem"])
+print(student.keys())
