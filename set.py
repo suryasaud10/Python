1c5e5bae34b79beb12e683db8d97f28ae0cc3455
Surya Saud <sssaud2058@gmail.com>
2025-05-15 22:48:24 +0545

    Add files via upload

---
+++ set.py
@@ -0,0 +1,16 @@
+collection = { "Renu" "Surya" "Saud"} 
+collection.add(1)
+collection.add("Joshi")
+collection.remove("Joshi")
+# collection.clear()
+# collection.pop()
+print(collection)
+print(type(collection))
+print(len(collection))
+call = set()
+print(type(call))
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+print(set1.union(set2))
+print(set1.intersection(set2))
